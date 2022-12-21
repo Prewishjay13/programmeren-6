797cead0acb0422290bcee9fa463c347c391824c
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2022-12-21 16:53:35 +0100

    route example

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 const express = require ('express')
 const dotenv = require('dotenv').config()
-const port = 5000
+const port = process.env.port || 5000 //dit gaat naar de .envfile bij de port variable 
 
 const app = express()
 
+//routes (should be in a seperate folder)
+//how to send a get request, this can be used in postman
+// app.get('/api/details', (req, res) => {
+//     res.send('details')
+// })
+// app.get('/api/details', (req, res) => {
+//     res.json({message: 'this is a details test'})
+// })
+app.get('/api/details', (req, res) => {
+    res.status(200).json({message: 'this is a details test'})
+})
 app.listen(port, () => console.log(`Server started on port ${port}`))
