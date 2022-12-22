378c1f3fc7831a26a855b17872343fc21e563fc0
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2022-12-22 08:26:54 +0100

    Update detailRoutes.js

--- backend/routes/detailRoutes.js
+++ backend/routes/detailRoutes.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const {getDetails,  setDetail, updateDetails, deleteDetail} = require('../controllers/detailController')
 
-router.get('/', getDetails)
+//router.get('/', getDetails)
 //send info
-router.post('/', setDetail)
+//router.post('/', setDetail)
 //update
-router.put('/:id', updateDetails)
-
-router.delete('/:id', deleteDetail)
-
+//router.put('/:id', updateDetails)
+//delete
+//router.delete('/:id', deleteDetail)
+router.route('/').get(getDetails).post(setDetail)
+router.route('/:id').delete(deleteDetail).put(updateDetails)
 module.exports = router
